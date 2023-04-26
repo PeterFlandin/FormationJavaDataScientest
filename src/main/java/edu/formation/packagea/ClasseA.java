4e61eb3c4893159d815c8af4f55ced7bf6a2b772
PeterFlandin <peterflandin@gmail.com>
2023-04-26 12:07:25 +0200

    ajout enum

--- src/main/java/edu/formation/packagea/ClasseA.java
+++ src/main/java/edu/formation/packagea/ClasseA.java
@@ -8,4 +8,16 @@ public class ClasseA {
         ClasseB.methodeB();
     }
 
+    public enum Mois{
+        janvier("janvier", 31), fevrier("fevrier", 28), mars("mars", 1000),avril("avril", 5000);
+
+        String mois;
+        int nnbJours;
+        Mois (String mois, int nnbJours) {
+            this.mois = mois;
+            this.nnbJours=nnbJours;
+        }
+
+    }
+
 }
---
+++ src/main/java/edu/formation/packagea/Main.java
@@ -0,0 +1,13 @@
+package edu.formation.packagea;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Mois 1 : "+ClasseA.Mois.janvier  +" "+ClasseA.Mois.janvier.nnbJours + " Mois 2 :" +ClasseA.Mois.fevrier + " "+ ClasseA.Mois.fevrier.nnbJours);
+
+    }
+
+
+}
