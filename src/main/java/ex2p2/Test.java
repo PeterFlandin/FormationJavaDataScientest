4067ea2e3ffbfcb070d80878186adabfc14d508a
PeterFlandin <peterflandin@gmail.com>
2023-04-27 14:32:37 +0200

    instant of

--- src/main/java/ex2p2/Test.java
+++ src/main/java/ex2p2/Test.java
@@ -4,30 +4,27 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Cercle cercle = new Cercle(20);
-        Carre carre = new Carre(50);
 
-        FormeGeometrique[] formTab;
+        FormeGeometrique[] ge = {new Carre(50), new Carre(50), new Cercle(20), new Cercle(20)};
 
-         formTab = new FormeGeometrique[1];
-         formTab[0] = cercle;
+        Carre tab1[] = new Carre[2];
+        Cercle tab2[] = new Cercle[2];
 
-        FormeGeometrique[] formTab2;
+int carre = 0;
+int cercle = 0;
 
-        formTab2 = new FormeGeometrique[1];
-        formTab2[0] = carre;
+        for (int i = 0; i < ge.length; i++) {
+            if (ge[i] instanceof Carre) {
+                tab1[carre] = (Carre) ge[i];
+                System.out.println(tab1[carre]);
+                carre++;
 
+            } else {
+                tab2[cercle] = (Cercle) ge[i];
+                System.out.println(tab2[cercle]);
+                cercle++;
 
-        for (FormeGeometrique form: formTab
-             ) {
-            System.out.println(form.getSuperficie());
+            }
         }
-
-        for (FormeGeometrique form2: formTab2) {
-            System.out.println(form2.getSuperficie());
-        }
-
-        }
-
     }
-
+}
