7464a28b700ce79ac2e896847574e70c4e8c97f3
PeterFlandin <peterflandin@gmail.com>
2023-04-28 15:18:37 +0200

    ajout vector ex2p2

--- src/main/java/ex2p2/Test.java
+++ src/main/java/ex2p2/Test.java
@@ -1,28 +1,25 @@
 package ex2p2;
 
+import java.util.Vector;
+
 public class Test {
 
     public static void main(String[] args) {
 
+        FormeGeometrique[] ge = {new Carre(50), new Carre(50), new Cercle(20), new Cercle(20), new Carre(50), new Cercle(20), new Cercle(20)};
 
-        FormeGeometrique[] ge = {new Carre(50), new Carre(50), new Cercle(20), new Cercle(20)};
+        Vector careeVec = new Vector();
+        Vector cercleVec = new Vector();
 
-        Carre tab1[] = new Carre[2];
-        Cercle tab2[] = new Cercle[2];
-
-int carre = 0;
-int cercle = 0;
 
         for (int i = 0; i < ge.length; i++) {
             if (ge[i] instanceof Carre) {
-                tab1[carre] = (Carre) ge[i];
-                System.out.println(tab1[carre]);
-                carre++;
+               careeVec.add(ge[i]);
+                System.out.println(careeVec);
 
             } else {
-                tab2[cercle] = (Cercle) ge[i];
-                System.out.println(tab2[cercle]);
-                cercle++;
+                cercleVec.add(ge[i]);
+                System.out.println(cercleVec);
             }
         }
     }
